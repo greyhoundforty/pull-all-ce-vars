3901776387182dd6fa30f2096ccb26919dbe4440
greyhoundforty <ryantiffany@fastmail.com>
2023-03-16 06:35:27 -0500

    Building new container image

--- main.py
+++ main.py
@@ -45,6 +45,81 @@ def getWorkspaceOutputs(instance):
     getWsOutput = (wsOutputs[0]['output_values'][0][instance]['value'])
     return getWsOutput
 
+def updateWorkspace():
+    client = schematicsClient()# Construct the terraform taint command model
+    terraform_command_model = {
+        'command': 'taint',
+        'command_params': 'random_integer.location',
+        'command_name': 'location-taint',
+        'command_desc': 'Run taint on location resource',
+    }
+
+    print("Tainting location resource in Workspace.")
+
+    wsUpdate = client.run_workspace_commands(
+        w_id=workspaceId,
+        refresh_token=refreshToken,
+        commands=[terraform_command_model],
+        operation_name='taint-location',
+        description='taint-location'
+    ).get_result()
+
+    updateActivityId = wsUpdate.get('activityid')
+
+    while True:
+        jobStatus = schematicsService.get_job(job_id=updateActivityId).get_result()['status']['workspace_job_status']['status_code']
+        if (jobStatus == 'job_in_progress' or jobStatus == 'job_pending'):
+            print("Workspace update in progress. Checking again in 30 seconds...")
+            time.sleep(30)
+        elif (jobStatus == 'job_cancelled' or jobStatus == 'job_failed'):
+            log.warning("Workspace update failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + updateActivityId)
+            print("Workspace update failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + updateActivityId)
+            break
+        else:
+            print("Workspace update complete. Proceeding to Workspace plan.")
+            break
+def planWorkspace():
+    client = schematicsClient()
+    wsPlan = client.plan_workspace_command(
+        w_id=workspaceId,
+        refresh_token=refreshToken,
+    ).get_result()
+
+    planActivityId = wsPlan.get('activityid')
+
+    while True:
+        planStatus = schematicsService.get_job(job_id=planActivityId).get_result()['status']['workspace_job_status']['status_code']
+        if (planStatus == 'job_in_progress' or planStatus == 'job_pending'):
+            print("Workspace plan in progress. Checking again in 30 seconds...")
+            time.sleep(30)
+        elif (planStatus == 'job_cancelled' or planStatus == 'job_failed'):
+            print("Workspace plan failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + planActivityId)
+            break
+        else:
+            print("Workspace plan complete. Proceeding to Workspace apply.")
+            break
+
+def applyWorkspace():
+    client = schematicsClient()
+    wsApply = client.apply_workspace_command(
+        w_id=workspaceId,
+        refresh_token=refreshToken,
+    ).get_result()
+
+    applyActivityId = wsApply.get('activityid')
+
+    while True:
+        applyStatus = schematicsService.get_job(job_id=applyActivityId).get_result()['status']['workspace_job_status']['status_code']
+        if (applyStatus == 'job_in_progress' or applyStatus == 'job_pending'):
+            print("Workspace apply in progress. Checking again in 1 minute...")
+            time.sleep(60)
+        elif (applyStatus == 'job_cancelled' or applyStatus == 'job_failed'):
+            print("Workspace apply failed. Please check the logs by running the following command: ibmcloud schematics job logs --id " + applyActivityId)
+            break
+        else:
+            print("Workspace apply complete. Gathering workspace outputs.")
+            break
+
 # Pull all Code Engine service variables
 # Any services that are bound to the code engine app/job are exposed as CE_SERVICES
 def pullallCeVars():
@@ -134,17 +209,44 @@ def etcdWrite(key, value):
 
 try:
     # allOutputs = pullAllWorkspaceOutputs()
-    print("Pulling Centos server ID from Schematics workspace output.")
-    print("Call is: centosServerId = getWorkspaceOutputs(instance='centos_server_id')")
-    centosServerId = getWorkspaceOutputs(instance='centos_server_id')
-    print("Centos Server ID " + centosServerId + " pulled. Now attempting to write to etcd instance.")
-    writeCentos = etcdWrite('/current-servers/centos_server_id', value=centosServerId)
-    print("Centos Server ID " + centosServerId + " written to etcd instance.") 
+    print("Pulling currently deployed server IDs from Schematics workspace output. These will be used to pull the open cancellation ticket and update it for immediate cancellation.")
+    currentCentosServerId = getWorkspaceOutputs(instance='centos_server_id')
+    currentUbuntuServerId = getWorkspaceOutputs(instance='ubuntu_server_id')
+    currentWindowsServerId = getWorkspaceOutputs(instance='windows_server_id')
+    print("Server IDs pulled from Schematics output.")
+    print("") 
+    print("Writing Server IDs to cancellation queue '/update-cancel-ticket/' in etcd.")
+    writeCentos = etcdWrite('/update-cancel-ticket/centos_server_id', value=currentCentosServerId)
+    writeUbuntu = etcdWrite('/update-cancel-ticket/ubuntu_server_id', value=currentUbuntuServerId)
+    writeWindows = etcdWrite('/update-cancel-ticket/windows_server_id', value=currentWindowsServerId)
+    print("Server IDs written to cancellation queue in etcd instance.")
+    print("Starting Workspace update")
+    updateWorkspace()
+    print("Workspace update complete. Proceeding to Workspace plan.")
+    planWorkspace()
+    print("Workspace plan complete. Proceeding to Workspace apply.")
+    applyWorkspace()
+    print("Workspace apply complete. Proceeding to Workspace output pull.")
+    print("Pulling newly deployed server IDs from Schematics workspace output.")
+    newCentosServerId = getWorkspaceOutputs(instance='centos_server_id')
+    newUbuntuServerId = getWorkspaceOutputs(instance='ubuntu_server_id')
+    newWindowsServerId = getWorkspaceOutputs(instance='windows_server_id')
+    print("Server IDs pulled from Schematics output.")
     print("")
-    print("Reading Centos Server ID from etcd instance.")
-    centosId = etcdRead(key='/current-servers/centos_server_id')
-    transformedId = centosId[0].decode('utf-8')
-    print("Centos Server ID pulled from etcd: " + str(transformedId))
+    print("Writing Server IDs to etcd instance.")
+    writeCentos = etcdWrite('/current-servers/centos_server_id', value=newCentosServerId)
+    writeUbuntu = etcdWrite('/current-servers/ubuntu_server_id', value=newUbuntuServerId)
+    writeWindows = etcdWrite('/current-servers/windows_server_id', value=newWindowsServerId)
+    print("Server IDs written to etcd instance.")
+
+    # print("Centos Server ID " + centosServerId + " pulled. Now attempting to write to etcd instance.")
+    # writeCentos = etcdWrite('/current-servers/centos_server_id', value=centosServerId)
+    # print("Centos Server ID " + centosServerId + " written to etcd instance.") 
+    # print("")
+    # print("Reading Centos Server ID from etcd instance.")
+    # centosId = etcdRead(key='/current-servers/centos_server_id')
+    # transformedId = centosId[0].decode('utf-8')
+    # print("Centos Server ID pulled from etcd: " + str(transformedId))
     
     # print("output type is: " + str(type(allOutputs)))
     # Everything below this is working 
@@ -156,6 +258,8 @@ try:
     # print(nonsenseId1)
     # transformedId = nonsenseId1[0].decode('utf-8')
     # print(transformedId)
-except KeyError():
-    print("Key error")
+except ApiException(code): 
+    print("Error: " + str(code))
+# except KeyError():
+#     print("Key error")
     
