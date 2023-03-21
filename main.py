8eecfaa897a678a6a793c8de53acba72e189854d
greyhoundforty <ryantiffany@fastmail.com>
2023-03-21 13:55:44 -0500

    Attempting direct COS write

--- main.py
+++ main.py
@@ -15,80 +15,59 @@ def cosPath():
     cosPath = today.strftime("%Y") + '/' + today.strftime("%m") + '/' + today.strftime("%d") + '/'
     return cosPath
 
-# # Set up IAM authenticator and pull refresh token
-# authenticator = IAMAuthenticator(
-#     apikey=os.environ.get('IBMCLOUD_API_KEY'),
-#     client_id='bx',
-#     client_secret='bx'
-#     )
+# Set up IAM authenticator and pull refresh token
+authenticator = IAMAuthenticator(
+    apikey=os.environ.get('IBMCLOUD_API_KEY'),
+    client_id='bx',
+    client_secret='bx'
+    )
 
-# refreshToken = authenticator.token_manager.request_token()['refresh_token']
+refreshToken = authenticator.token_manager.request_token()['refresh_token']
 
-# Pull all Code Engine service variables
-# # Any services that are bound to the code engine app/job are exposed as CE_SERVICES
-# def pullallCeVars():
-#     ceVars = os.environ.get('CE_SERVICES')
-#     ceVarsToJson = json.loads(ceVars)
-#     allVars  = list(ceVarsToJson.values())    
-#     return  allVars
+def logDnaLogger():
+    key = os.environ.get('LOG_INGESTION_KEY')
+    log = logging.getLogger('logdna')
+    log.setLevel(logging.INFO)
 
-# def getLogDNAIngestionKey():
-#     allVars = pullallCeVars()
-#     ingestionKey = allVars[1][0]['credentials']['ingestion_key']
-#     return ingestionKey
+    options = {
+        'app': 'pull-all-ce-vars',
+        'url': 'https://logs.private.us-south.logging.cloud.ibm.com/logs/ingest',
+        'index_meta': True,
+        'tags': 'pull-all-ce-vars'
+    }
 
-# def logDnaLogger():
-#     key = getLogDNAIngestionKey()
-#     # key = os.environ.get('LOGDNA_INGESTION_KEY')
-#     log = logging.getLogger('logdna')
-#     log.setLevel(logging.INFO)
+    logger = LogDNAHandler(key, options)
+    log.addHandler(logger)
+    return log
 
-#     options = {
-#         'app': 'pull-all-ce-vars',
-#         'url': 'https://logs.private.us-south.logging.cloud.ibm.com/logs/ingest',
-#     }
+def cosClient():
+    cosInstanceCrn = os.environ.get('CLOUD_OBJECT_STORAGE_RESOURCE_INSTANCE_ID')
+    cosApiKey = os.environ.get('CLOUD_OBJECT_STORAGE_APIKEY')
+    cosEndpoint = 'https://s3.direct.us-south.cloud-object-storage.appdomain.cloud'
+    # cosEndpoint = ("https://" + os.environ.get('COS_ENDPOINT'))
+    cos = ibm_boto3.resource("s3",
+        ibm_api_key_id=cosApiKey,
+        ibm_service_instance_id=cosInstanceCrn,
+        config=Config(signature_version="oauth"),
+        endpoint_url=cosEndpoint
+    )
+    return cos
 
-#     logger = LogDNAHandler(key, options)
-#     log.addHandler(logger)
-#     return log
+def writeCosFile():
+    client = cosClient()
+    haikunator = Haikunator()
+    basename = haikunator.haikunate(token_length=0, delimiter='')
+    cosBucket = "dummy-us-south-cancel-bucket"
+    cosFile = cosPath + basename + ".txt"
+    cosFileContents = basename
 
-# # Retrieve COS service variables
-# def getCosCeVars():
-#     allVars = pullallCeVars()
-#     cosVars = allVars[0][0]
-#     return cosVars
+    print("Writing test content " + basename + " to file at: " + cosFile)
+    client.Object(cosBucket, cosFile).put(Body=cosFileContents)
 
-# def cosClient():
-#     pullCosVars = getCosCeVars()
-#     cosInstanceCrn = pullCosVars['credentials']['resource_instance_id']
-#     cosApiKey = pullCosVars['credentials']['apikey']
- #cosEndpoint = 'https://s3.direct.us-south.cloud-object-storage.appdomain.cloud'
-#     cosEndpoint = ("https://" + os.environ.get('COS_ENDPOINT'))
-#     cos = ibm_boto3.resource("s3",
-#         ibm_api_key_id=cosApiKey,
-#         ibm_service_instance_id=cosInstanceCrn,
-#         config=Config(signature_version="oauth"),
-#         endpoint_url=cosEndpoint
-#     )
-#     return cos
-
-# def writeCosFile():
-#     client = cosClient()
-#     haikunator = Haikunator()
-#     basename = haikunator.haikunate(token_length=0, delimiter='')
-#     log = logDnaLogger()
-#     cosBucket = "dummy-us-south-cancel-bucket"
-#     cosFile = cosPath + "test.txt"
-#     cosFileContents = basename
-
-#     log.info("Writing test content " + basename + " to file at: " + cosFile)
-#     client.Object(cosBucket, cosFile).put(Body=cosFileContents)
-
-# def listBuckets():
-#     client = cosClient()
-#     log = logDnaLogger()
-#     for bucket in client.buckets.all():
-#         log.info(bucket.name)
+def listBuckets():
+    client = cosClient()
+    for bucket in client.buckets.all():
+        print(bucket.name)
 
 
 # Useful for debugging, prints all environment variables
@@ -97,7 +76,10 @@ def getAllVars():
         print("{0}: {1}".format(name, value))
 
 try:
-    print("Pulling all Code Engine Environment variables")
-    getAllVars()
+    log = logDnaLogger()
+    log.debug("Attempting to write file to COS")
+    writeCosFile()
+    log.debug("Attempting to list buckets")
+    listBuckets()
 except Exception as e:
-    print("Error: " + str(e))
+    log.error("Error: " + str(e))
